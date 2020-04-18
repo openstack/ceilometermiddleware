0b6708e986f3df41854d34f47d9e6e26d3884748
Sean McGinnis <sean.mcginnis@gmail.com>
2020-04-18 11:52:18 -0500

    Use unittest.mock instead of third party mock
    Now that we no longer support py27, we can use the standard library
    unittest.mock module instead of the third party mock lib.
    
    Change-Id: I64f6f5df4d82faef205e5019be5a41c1b1f1f1fa
    Signed-off-by: Sean McGinnis <sean.mcginnis@gmail.com>

--- ceilometermiddleware/tests/test_swift.py
+++ ceilometermiddleware/tests/test_swift.py
@@ -13,8 +13,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 import threading
+from unittest import mock
 
-import mock
 from oslo_config import cfg
 import six
 
